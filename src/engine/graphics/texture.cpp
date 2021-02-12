1ed632d34e162896ec8d5e318aa644155f39c516
iirthw <iirthw@gmail.com>
2021-02-12 22:43:28 +0200

    Work on texture impl (part 2)

--- src/engine/graphics/texture.cpp
+++ src/engine/graphics/texture.cpp
@@ -25,7 +25,26 @@ namespace urus
 
     void Texture::load(const char* path)
     {
-        // FIXME: missing impl
+		glBindTexture(GL_TEXTURE_2D, mHandle);
+		// FIXME: correct the image loading part
+		int width, height, channels;
+		// unsigned char* data = stbi_load(path, &width, &height, &channels, 4);
+		
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		// FIXME: free the image data
+		// stbi_image_free(data);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		
+		glBindTexture(GL_TEXTURE_2D, 0);
+		mWidth = width;
+		mHeight = height;
+		mChannels = channels;
     }
 
     void Texture::set(unsigned int unformIndex, unsigned int textureIndex)
