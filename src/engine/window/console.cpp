c4424025a991d89c171ebc61a83ce888feaa9849
iirthw <iirthw@gmail.com>
2021-02-13 12:11:03 +0200

    [window] Fix cross-platform include

--- src/engine/window/console.cpp
+++ src/engine/window/console.cpp
@@ -1,5 +1,8 @@
 #ifndef NDEBUG
+
+#ifdef _WIN64
 #include <windows.h>
+#endif
 
 #include "console.h"
 
@@ -8,8 +11,8 @@ using namespace urus;
 void Console::updateWindowPositionAndBounds() const
 {
 #ifdef _WIN64
-	HWND handleWnd = GetConsoleWindow();
-	MoveWindow(handleWnd, mX, mY, mWidth, mHeight, TRUE /* repaint */);
+    HWND handleWnd = GetConsoleWindow();
+    MoveWindow(handleWnd, mX, mY, mWidth, mHeight, TRUE /* repaint */);
 #endif
 }
-#endif
+#endif // ifndef NDEBUG
