39509d89a4b556c95ecfeb31596da1dc66d563f5
iirthw <iirthw@gmail.com>
2021-03-01 21:20:15 +0200

    [macaque] Add isZero helper function

--- src/macaque/mq_math.h
+++ src/macaque/mq_math.h
@@ -7,4 +7,9 @@
 namespace macaque
 {
     static constexpr float MQ_EPSILON = 1.0e-7;
+
+    inline bool isZero(float x)
+    {
+        return (x < MQ_EPSILON) && (x > -MQ_EPSILON);
+    }
 } // namespace macaque
