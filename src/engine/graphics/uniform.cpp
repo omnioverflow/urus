0d9761b427e2518a8941b346764d2239d45fb0c0
iirthw <iirthw@gmail.com>
2021-03-07 21:25:26 +0200

    [graphics] Fix for Uniform explicit template specializations for ivec* and vec* types

--- src/engine/graphics/uniform.cpp
+++ src/engine/graphics/uniform.cpp
@@ -9,18 +9,15 @@
 
 namespace urus
 {
-    // FIXME: define integer vector, quaternion and mat4
-    // template specializations:
     template Uniform<int>;
-    // template Uniform<ivec2>;
-    // template Uniform<ivec4>;
     template Uniform<float>;
-    // FIXME: plug back uniform implementations for vec2, vec3, vec4
-    // FIXME: template Uniform<macaque::vec2>;
-    // FIXME: template Uniform<macaque::vec3>;
-    // FIXME: template Uniform<macaque::vec4>;
+    template Uniform<macaque::ivec2>;
+    template Uniform<macaque::ivec4>;
+    template Uniform<macaque::vec2>;
+    template Uniform<macaque::vec3>;
+    template Uniform<macaque::vec4>;
 
-
+    // FIXME: quaternion and mat4 template specializations:
     // template Uniform<quat>;
     //template Uniform<mat4>;
     
@@ -38,7 +35,10 @@ namespace urus
             set(index, data.data(), dataLength);
     }
 
-// A helper macro to reduce boilerplate code of setting uniforms of different types
+/**
+* A helper macro to reduce boilerplate code of setting uniforms of different types
+* (explicit specialization tailored for basic data types)
+*/
 #define UNIFORM_IMPL(gl_func, T, DataType) \
     template <> \
     void Uniform<T>::set(GLint index, const T* data, GLsizei dataLength) \
@@ -47,17 +47,30 @@ namespace urus
             gl_func(index, (GLsizei)dataLength, &data[0]); \
     }
 
+/**
+* Helper macro (similar to UNIFORM_IMPL), but tailored for vector data types 
+* (i.e. vec*, ivec*)
+* In current impl, a workaround with dereferencing (*data)[0] is used to call overloaded 
+* operator[] instead of built-in indexer on the pointer (c-style array issue)
+*/
+
+#define UNIFORM_VEC_IMPL(gl_func, T, DataType) \
+    template <> \
+    void Uniform<T>::set(GLint index, const T* data, GLsizei dataLength) \
+    { \
+        if (dataLength) \
+            gl_func(index, (GLsizei)dataLength, &((*data)[0])); \
+    }
+    
     UNIFORM_IMPL(glUniform1iv, int, int)
-
-    // UNIFORM_IMPL(glUniform2iv, ivec2, int)
-    // UNIFORM_IMPL(glUniform4iv, ivec4, int)
-
     UNIFORM_IMPL(glUniform1fv, float, float)
-    // FIXME: plug back uniform implementations for vec2, vec3, vec4
-    // UNIFORM_IMPL(glUniform2fv, macaque::vec2, float)
-    // UNIFORM_IMPL(glUniform3fv, macaque::vec3, float)
-    // UNIFORM_IMPL(glUniform4fv, macaque::vec4, float)
+    UNIFORM_VEC_IMPL(glUniform2iv, macaque::ivec2, int)
+    UNIFORM_VEC_IMPL(glUniform4iv, macaque::ivec4, int)
+    UNIFORM_VEC_IMPL(glUniform2fv, macaque::vec2, float)
+    UNIFORM_VEC_IMPL(glUniform3fv, macaque::vec3, float)
+    UNIFORM_VEC_IMPL(glUniform4fv, macaque::vec4, float)
 
+    // FIXME: fix quat & mat4 implementations
     // UNIFORM_IMPL(glUniform4fv, quat, float)
 
     // FIXME: implement all UNIFORM_IMPLs
