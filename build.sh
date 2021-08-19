b39e8dc743298fda42a762f8bb05f76ada930285
iirthw <iirthw@gmail.com>
2021-08-19 23:42:35 +0300

    [build] Fix result message after cmake command

--- build.sh
+++ build.sh
@@ -11,19 +11,35 @@ fi
 
 cd build
 
+successMessage="OK"
+failureMessage="KO (failure)"
 if [[ $OSTYPE == "darwin"* ]]; then
 	# Mac OSX
 	echo "[ ******** -- Generating Xcode project from cmake..."
-    cmake -G Xcode ..
-	echo "  ******** --> OK ]"
+	cmake -G Xcode ..
+
+	result=$? # result of the most recent command
+	resultMessage=$failureMessage
+	if [ $result -eq 0 ]; then
+		resultMessage=$successMessage
+    fi # check result
+
+    echo "  ******** --> " $resultMessage " ]"
 elif [[ $OSTYPE == "msys" ]]; then # for Git bash on Windows
-    echo "[ ******** -- Generating Visual Studio solution from cmake..."
+echo "[ ******** -- Generating Visual Studio solution from cmake..."
 	# TODO: create some support for Cygwin 
-    cmake -G "Visual Studio 16 2019" ..
-	echo "  ******** --> OK ]"
+	cmake -G "Visual Studio 16 2019" ..
+
+	result=$?
+	resultMessage=$failureMessage
+	if [ $result -eq 0 ]; then
+		resultMessage=$successMessage
+    fi # check result
+
+    echo "  ******** --> " $resultMessage "]"
 else 
-    echo "[ ******** /!\ Only Mac OSX and Windows x64 are supported\n"
-	echo "  ******** --> KO ]"
+	echo "[ ******** /!\ Only Mac OSX and Windows x64 are supported\n"
+	echo "  ******** --> " $failureMessage " ]"
 	cd ..
 	exit 1 
 fi 
