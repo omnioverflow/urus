64cbd7e987d84ad4694b4c97a3fda5876b37a176
iirthw <iirthw@gmail.com>
2021-09-14 23:09:58 +0300

    wip: fix in Mat3

--- src/mqMath/Mat3.cpp
+++ src/mqMath/Mat3.cpp
@@ -2,79 +2,65 @@
 
 namespace mq {
 
-        mat3(const GLfloat d = GLfloat(1.0f)) {
-            m_[0].x = d;
-            m_[1].y = d;
-            m_[2].z = d;
-        }
+        explicit mat3(const GLfloat d = GLfloat(1.0f))
+        : mat3(vec3(d, 0.f, 0.f), vec3(0.f, d, 0.f), vec3(0.f, 0.f, d)) {}
 
-        mat3(const vec3& a, const vec3& b, const vec3& c) {
-            m_[0] = a;
-            m_[1] = b;
-            m_[2] = c;
-        }
+        mat3(const vec3& row0, const vec3& row1, const vec3& row2)
+        : m_{a, b, c} {}
 
         mat3(GLfloat m00, GLfloat m10, GLfloat m20,
             GLfloat m01, GLfloat m11, GLfloat m21,
-            GLfloat m02, GLfloat m12, GLfloat m22) {
-            m_[0] = vec3(m00, m01, m02);
-            m_[1] = vec3(m10, m11, m12);
-            m_[2] = vec3(m20, m21, m22);
-        }
+            GLfloat m02, GLfloat m12, GLfloat m22)
+            : mat3(vec3(m00, m01, m02), vec3(m10, m11, m12), vec3(m20, m21, m22)
+            {}
 
-        mat3(const mat3& m) {
-            if (*this != m) {
-                m_[0] = m.m_[0];
-                m_[1] = m.m_[1];
-                m_[2] = m.m_[2];
-            }
-        }
-
-        // --------------------------------------------------------------------
-        //  Indexing Operator
-        // --------------------------------------------------------------------
+        mat3(const mat3& other)
+        : mat3(other[0], other[1], other[2]) {}
 
         vec3& operator[] (int i) { return m_[i]; }
         const vec3& operator[] (int i) const { return m_[i]; }
 
-        // --------------------------------------------------------------------
-        //  (non-modifying) Arithmatic Operators
-        // --------------------------------------------------------------------
-
-        mat3& operator+= (const mat3& m) {
-            m_[0] += m[0];
-            m_[1] += m[1];
-            m_[2] += m[2];
+        mat3& operator+= (const mat3& rhs) {
+            m_[0] += rhs[0];
+            m_[1] += rhs[1];
+            m_[2] += rhs[2];
             return *this;
         }
 
         mat3 operator+ (const mat3& rhs) const {
-            return mat3(m_[0] + m[0], m_[1] + m[1], m_[2] + m[2]);
+            auto temp(*this);
+            temp += rhs;
+            return temp;
         }
 
-        mat3& operator-= (const mat3& m) {
-            m_[0] -= m[0];
-            m_[1] -= m[1];
-            m_[2] -= m[2];
+        mat3& operator-= (const mat3& rhs) {
+            m_[0] -= rhs[0];
+            m_[1] -= rhs[1];
+            m_[2] -= rhs[2];
             return *this;
         }
 
-        mat3 operator- (const mat3& m) const {
-            return mat3(m_[0] - m[0], m_[1] - m[1], m_[2] - m[2]);
+        mat3 operator- (const mat3& rhs) const {
+            auto temp(*this);
+            temp -= rhs;
+            return temp;
         }
 
-        mat3& operator*= (const mat3& m) {
-            mat3  a(0.0f);
-
+        mat3& operator*= (const mat3& rhs) {            
             for (int i = 0; i < 3; ++i) {
                 for (int j = 0; j < 3; ++j) {
                     for (int k = 0; k < 3; ++k) {
-                        a[i][j] += m_[i][k] * m[k][j];
+                        (*this)[i][j] += (*this)[i][k] * rhs[k][j];
                     }
                 }
             }
+            return *this;
+        }
 
-            return *this = a;
+        mat3 operator* (const mat3& rhs) const {
+            auto temp(*this);
+            temp *= rhs;
+            return temp;
         }
 
         mat3& operator*= (const GLfloat s) {
@@ -85,26 +71,14 @@ namespace mq {
         }
 
         mat3 operator* (const GLfloat s) const {
-            return mat3(s * m_[0], s * m_[1], s * m_[2]);
+            auto temp(*this);
+            temp *= s;
+            return temp;
         }        
 
-        friend mat3 operator* (const GLfloat s, const mat3& m) {
+        mat3 operator* (const GLfloat s, const mat3& rhs) {
             return m * s;
-        }
-
-        mat3 operator* (const mat3& m) const {
-            mat3  a(0.0f);
-
-            for (int i = 0; i < 3; ++i) {
-                for (int j = 0; j < 3; ++j) {
-                    for (int k = 0; k < 3; ++k) {
-                        a[i][j] += m_[i][k] * m[k][j];
-                    }
-                }
-            }
-
-            return a;
-        }
+        }        
 
         mat3& operator/= (const GLfloat s) {
             #ifdef DEBUG
@@ -116,20 +90,13 @@ namespace mq {
             #endif // DEBUG
 
             GLfloat r = GLfloat(1.0f) / s;
-            return *this *= r;
+            return (*this) *= r;
         }    
         
         mat3 operator/ (const GLfloat s) const {
-            #ifdef DEBUG
-            if (std::fabs(s) < DivideByZeroTolerance) {
-                std::cerr << "[" << __FILE__ << ":" << __LINE__ << "] "
-                    << "Division by zero" << std::endl;
-                return mat3();
-            }
-            #endif // DEBUG
-
-            GLfloat r = GLfloat(1.0f) / s;
-            return *this * r;
+            auto temp(*this);
+            temp /= s;
+            return temp;
         }
 
         // --------------------------------------------------------------------
