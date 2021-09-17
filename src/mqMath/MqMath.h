7d50fe4c0c07234c7a0cf1d32ad1443f3714150b
iirthw <iirthw@gmail.com>
2021-09-17 22:40:19 +0300

    [mqMath] [style] minor fix of style

--- src/mqMath/MqMath.h
+++ src/mqMath/MqMath.h
@@ -9,12 +9,11 @@ namespace mq
     namespace internal
     {
         constexpr float MQ_EPSILON = 1.0e-7;
-        constexpr float MQ_PI = 3.14159265359;
+        constexpr float MQ_PI = 3.14159265359f;
         constexpr float MQ_RADIANS_PER_DEGREE = MQ_PI / 180.f;
     }
     
-    inline bool isZero(float x)
-    {
+    inline bool isZero(float x) {
         return (x < internal::MQ_EPSILON) && (x > -internal::MQ_EPSILON);
     }
 } // namespace mq
