baa61df8e36c52ec28d1c4d1eeeacb2acaf4e5e9
iirthw <iirthw@gmail.com>
2021-09-18 22:42:35 +0300

    [mqMath] [style] better var naming

--- src/mqMath/Mat4.cpp
+++ src/mqMath/Mat4.cpp
@@ -147,27 +147,27 @@ namespace mq {
     // ------------------------------------------------------------------------
     
     mat4 mat4::rotateX(float thetaRadians) {
-        mat4 c;
-        c[2][2] = c[1][1] = cos(thetaRadians);
-        c[2][1] = sin(thetaRadians);
-        c[1][2] = -c[2][1];
-        return c;
+        mat4 temp;
+        temp[2][2] = temp[1][1] = cos(thetaRadians);
+        temp[2][1] = sin(thetaRadians);
+        temp[1][2] = -temp[2][1];
+        return temp;
     }
 
     mat4 mat4::rotateY(float thetaRadians) {
-        mat4 c;
-        c[2][2] = c[0][0] = cos(thetaRadians);
-        c[0][2] = sin(thetaRadians);
-        c[2][0] = -c[0][2];
-        return c;
+        mat4 temp;
+        temp[2][2] = temp[0][0] = cos(thetaRadians);
+        temp[0][2] = sin(thetaRadians);
+        temp[2][0] = -temp[0][2];
+        return temp;
     }
 
     mat4 mat4::rotateZ(float thetaRadians) {
-        mat4 c;
-        c[0][0] = c[1][1] = cos(thetaRadians);
-        c[1][0] = sin(thetaRadians);
-        c[0][1] = -c[1][0];
-        return c;
+        mat4 temp;
+        temp[0][0] = temp[1][1] = cos(thetaRadians);
+        temp[1][0] = sin(thetaRadians);
+        temp[0][1] = -temp[1][0];
+        return temp;
     }
 
     mat4 mat4::rotateXDegrees(float thetaDegrees) {
@@ -183,11 +183,11 @@ namespace mq {
     }
 
     mat4 mat4::translate(float x, float y, float z) {
-        mat4 c;
-        c[0][3] = x;
-        c[1][3] = y;
-        c[2][3] = z;
-        return c;
+        mat4 temp;
+        temp[0][3] = x;
+        temp[1][3] = y;
+        temp[2][3] = z;
+        return temp;
     }
 
     mat4 mat4::translate(const vec3& v) {
@@ -199,11 +199,11 @@ namespace mq {
     }
 
     mat4 mat4::scale(float x, float y, float z) {
-        mat4 c;
-        c[0][0] = x;
-        c[1][1] = y;
-        c[2][2] = z;
-        return c;
+        mat4 temp;
+        temp[0][0] = x;
+        temp[1][1] = y;
+        temp[2][2] = z;
+        return temp;
     }
 
     mat4 mat4::scale(const vec3& v) {
