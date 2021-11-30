188ce9077d1f8767edcfc59bae29346eceb92bd2
iirthw <iirthw@gmail.com>
2021-11-30 22:33:22 +0200

    [macros] fix unused macro: cast to void

--- src/engine/base/Macros.h
+++ src/engine/base/Macros.h
@@ -12,9 +12,5 @@
 // Macros to fix unused result/variable warning
 #ifdef UNUSED
 #elif defined(__GNUC__)
-# define UNUSED(x) UNUSED_ ## x __attribute__((unused))
-#elif defined(__LCLINT__)
-# define UNUSED(x) /*@unused@*/ x
-#else
-#define UNUSED(x) x
+# define UNUSED(x) (void)x
 #endif
